9e8084a0cb11c4580e6be7ec48d55aa65fde4eba
zhiqing <1775840762@qq.com>
2023-12-15 22:30:33 +0800

    最近创建的语录会有更大概率随机到

--- get_new_quote_with_bg.php
+++ get_new_quote_with_bg.php
@@ -6,7 +6,7 @@ $con = new Sql($db_host, $db_user, $db_pwd);
 $con->connect_mysql($db_database);
 $con->charset();
 
-$sql = "SELECT txt FROM `rains_own` ORDER BY RAND() LIMIT 1";
+$sql = "SELECT txt FROM `rains_own` ORDER BY RAND() * UNIX_TIMESTAMP(`time`) DESC  LIMIT 1";
 $query = $con->query($sql);
 $list = $query->fetchAll();
 
